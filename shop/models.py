f31961f7c657b74c57671052cc2befaf9a09d8ee
plany1777 <turboroshka@gmail.com>
2025-05-16 18:20:12 +0500

    first

---
+++ shop/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/', blank=True, null=True)  # добавлено
+
+    def __str__(self):
+        return self.name
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.product.name}"
